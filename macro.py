0f6b95923a0d0c6abf25eaa2aefee092030b9b81
Victor Erbs <145703555+VictorErbs@users.noreply.github.com>
2024-02-15 16:16:58 -0300

    Update macro.py

--- macro.py
+++ macro.py
@@ -2,9 +2,13 @@ import time
 import keyboard
 
 def press_f5():
+    """
+    Pressiona a tecla F5 a cada 2 segundos.
+    """
     while True:
         keyboard.press('F5')
         keyboard.release('F5')
         time.sleep(2)
 
-press_f5()
+if __name__ == "__main__":
+    press_f5()
